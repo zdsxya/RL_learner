6f39f70ee3d92716d12c80bdb1098050472e75c9
xy <18811086991@163.com>
2025-11-19 17:30:06 +0800

    sarsa_tensorboard,Q-learning

--- Q-learning.py
+++ Q-learning.py
@@ -1,41 +1,39 @@
-import numpy as np
 import gymnasium as gym
+import numpy as np
 import time
-import gridworld
 from torch.utils.tensorboard import SummaryWriter
 
-
-class SARSAAgent:
-    def __init__(self, n_states, n_act, episilon=0.1,lr=0.1,gamma=0.9):
+class QLearningAgent:
+    def __init__(self, n_states, n_act, epsilon=0.1, lr=0.1, gamma=0.9):
         self.Q = np.zeros((n_states, n_act))
-        self.episilon = episilon
         self.n_act = n_act
+        self.epsilon = epsilon
         self.lr = lr
         self.gamma = gamma
 
     def predict(self, state):
         Q_list = self.Q[state, :]
-        # action = np.argmax(Q_list)
-        action = np.random.choice(np.flatnonzero(Q_list == Q_list.max())) # 处理多个最大值
+        action = np.random.choice(np.flatnonzero(Q_list == Q_list.max()))
+        return action
+
+    def act(self, state):
+        if np.random.uniform(0,1) < self.epsilon:
+            action = np.random.choice(self.n_act)
+        else:
+            action = self.predict(state)
         return action
     
-    def act(self, state):
-        if np.random.uniform(0, 1) < self.episilon: 
-            action = np.random.choice(self.n_act)
-        else: 
-            action = self.predict(state)       
-        return action 
-
-    def learn(self, state, action, reward, next_state, next_action, done):
+    def learn(self, state, action, reward, next_state, done):
         current_Q = self.Q[state, action]
        
         if done:
             target_Q = reward
         else:
-            target_Q = self.gamma * self.Q[next_state, next_action] + reward
+            target_Q = reward + self.gamma * np.max(self.Q[next_state, :])
+
         self.Q[state, action] += self.lr * (target_Q - current_Q)
 
-def train_episodes(env, agent, is_render):
+def train_episodes(env, agent):
     total_reward = 0
     state, _ = env.reset()
     action = agent.act(state)
@@ -43,18 +41,17 @@ def train_episodes(env, agent, is_render):
     while True:
         next_state, reward, done, _ , info = env.step(action)
         next_action = agent.act(next_state)
-        agent.learn(state, action, reward, next_state, next_action, done)
-
+        agent.learn(state, action, reward, next_state, done)
 
         state = next_state
         action = next_action
         total_reward += reward
+        # time.sleep(0.5)
 
-        if is_render:
-            env.render()
         if done:
             break
     return total_reward
+
 
 def test_episodes(env, agent):
     total_reward = 0
@@ -62,44 +59,30 @@ def test_episodes(env, agent):
 
     while True:
         action = agent.predict(state)
-        next_state, reward, done, _ , info= env.step(action)
-       
+        next_state, reward, done, _ , info = env.step(action)
         state = next_state
         total_reward += reward
-        # env.render()
-        time.sleep(0.5)
         if done:
             break
-    
+
     return total_reward
 
-def train(env,episodes=500,episilon=0.1,lr=0.1,gamma=0.9):
-    writer = SummaryWriter()
-    agent = SARSAAgent(
+def train(env, episodes=500, epsilon=0.1, lr=0.1, gamma=0.9):
+    agent = QLearningAgent(
         n_states = env.observation_space.n,
         n_act = env.action_space.n,
         lr = lr,
         gamma = gamma,
-        episilon = episilon
+        epsilon = epsilon
     )
 
-    is_render = False
     for e in range(episodes):
-        ep_reward = train_episodes(env, agent, is_render)
+        ep_reward = train_episodes(env, agent)
         print('Episode %s: Reward = %.1f' % (e, ep_reward))
-        writer.add_scalar("Reward/train", ep_reward, e)
-
-        if e % 50 == 0:
-            is_render = True
-        else:
-            is_render = False
     
     test_reward = test_episodes(env, agent)
-    print('test_reward = %.1f' % (test_reward))
+    print('test_reward = %.1f' % test_reward)
 
-    writer.close()
-        
 if __name__ == '__main__':
     env = gym.make("CliffWalking-v1")
-    # env = gridworld.CliffWalkingWapper(env)
     train(env)
